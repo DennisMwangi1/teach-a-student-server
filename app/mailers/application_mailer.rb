ae3c50f6f5242f23cee05bd3a3d5096fe0b075a7
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-10-13 10:02:08 +0300

    configured mailers for informaation that will be sent to the parents email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'teach.a.student1@gmail.com'
   layout 'mailer'
 end
 
--- app/mailers/register_mailer.rb
+++ app/mailers/register_mailer.rb
@@ -1,2 +1,11 @@
 class RegisterMailer < ApplicationMailer
+     default from: 'teach.a.student1@gmail.com'
+
+  def welcome_email
+    @user = params[:user]
+    @student=params[:student]
+    @url  = 'http://teach-a-student.com'
+    mail(to: @user.email, subject: 'Welcome to Teach-A-Student')
+  end
+  
 end
