b42d17b68602e2394aabc792fa86990ada54d76d
DennisMwangi1 <dennis.kamau@student.moringaschool.com>
2022-10-14 14:44:37 +0300

    added routes for actions

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   resources :students
   post '/studentlogin', to:"studentsessions#create"
   post 'parentlogin', to:"parentsessions#create"
+  post 'teacherlogin', to:"teachersessions#create"
   get '/me' ,to:"students#show"
+  get '/teacherme' ,to:"teachers#show"
   delete '/studentlogout', to:"studentsessions#destroy"
   delete '/parentlogout', to:"parentsessions#destroy"
   post '/studentsignup', to: 'students#create'
   post '/parentsignup', to:'parents#create'
+  post '/teachersignup', to:'teachers#create'
+  post '/requestsession', to: 'requested_sessions#create'
+  post '/acceptsession', to: 'accepted_sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
